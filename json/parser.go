cbe30a6c499c2a1a3c76ad2938e3dae62fb75f87
thepoy <thepoy@163.com>
2021-10-20 21:37:22 +0800

    use the type `JSONResult` to replace `gjson.Result` to reduce package references

--- json/parser.go
+++ json/parser.go
@@ -3,13 +3,21 @@
  * @Email: thepoy@163.com
  * @File Name: parser.go
  * @Created: 2021-07-27 20:41:02
- * @Modified: 2021-10-12 09:44:25
+ * @Modified: 2021-10-20 21:35:14
  */
 
 package json
 
 import "github.com/tidwall/gjson"
 
-func ParseJSON(body []byte) gjson.Result {
+type JSONResult = gjson.Result
+
+// ParseBytesToJSON converts `[]byte` variable to JSONResult
+func ParseBytesToJSON(body []byte) JSONResult {
 	return gjson.ParseBytes(body)
+}
+
+// ParseBytesToJSON converts `string` variable to JSONResult
+func ParseJSON(body string) JSONResult {
+	return gjson.Parse(body)
 }
