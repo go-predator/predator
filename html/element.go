5288f4fcca6e21afabfd99e9afdeeb589ca26d60
thepoy <thepoy@163.com>
2021-10-26 18:14:44 +0800

    fix the issue of no abnormal exit caused by empty `HTMLElement` executing some method

--- html/element.go
+++ html/element.go
@@ -3,7 +3,7 @@
  * @Email: thepoy@163.com
  * @File Name: element.go
  * @Created: 2021-07-27 20:35:31
- * @Modified: 2021-10-10 16:25:14
+ * @Modified: 2021-10-26 18:10:59
  */
 
 package html
@@ -43,7 +43,7 @@ func NewHTMLElementFromSelectionNode(s *goquery.Selection, n *html.Node, index i
 
 // Attr returns the selected attribute of a HTMLElement or empty string
 // if no attribute found
-func (he HTMLElement) Attr(key string) string {
+func (he *HTMLElement) Attr(key string) string {
 	for _, attr := range he.Node.Attr {
 		if attr.Key == key {
 			return attr.Val
@@ -55,31 +55,31 @@ func (he HTMLElement) Attr(key string) string {
 // OuterHtml returns the outer HTML rendering of the first item in
 // the selection - that is, the HTML including the first element's
 // tag and attributes.
-func (he HTMLElement) OuterHTML() (string, error) {
+func (he *HTMLElement) OuterHTML() (string, error) {
 	return goquery.OuterHtml(he.DOM)
 }
 
 // InnerHTML gets the HTML contents of the first element in the set of matched
 // elements. It includes text and comment nodes.
-func (he HTMLElement) InnerHTML() (string, error) {
+func (he *HTMLElement) InnerHTML() (string, error) {
 	return he.DOM.Html()
 }
 
 // Text gets the combined text contents of each element in the set of matched
 // elements, including their descendants.
-func (he HTMLElement) Text() string {
+func (he *HTMLElement) Text() string {
 	return he.DOM.Text()
 }
 
 // ChildText returns the concatenated and stripped text content of the matching
 // elements.
-func (he HTMLElement) ChildText(selector string) string {
+func (he *HTMLElement) ChildText(selector string) string {
 	return strings.TrimSpace(he.DOM.Find(selector).Text())
 }
 
 // ChildrenText returns the stripped text content of all the matching
 // elements.
-func (he HTMLElement) ChildrenText(selector string) []string {
+func (he *HTMLElement) ChildrenText(selector string) []string {
 	var res []string
 	he.DOM.Find(selector).Each(func(_ int, s *goquery.Selection) {
 		res = append(res, strings.TrimSpace(s.Text()))
@@ -89,7 +89,7 @@ func (he HTMLElement) ChildrenText(selector string) []string {
 
 // ChildAttr returns the stripped text content of the first matching
 // element's attribute.
-func (he HTMLElement) ChildAttr(selector, attrName string) string {
+func (he *HTMLElement) ChildAttr(selector, attrName string) string {
 	if attr, ok := he.DOM.Find(selector).Attr(attrName); ok {
 		return strings.TrimSpace(attr)
 	}
@@ -98,7 +98,7 @@ func (he HTMLElement) ChildAttr(selector, attrName string) string {
 
 // ChildrenAttr returns the stripped text content of all the matching
 // element's attributes.
-func (he HTMLElement) ChildrenAttr(selector, attrName string) []string {
+func (he *HTMLElement) ChildrenAttr(selector, attrName string) []string {
 	var res []string
 	he.DOM.Find(selector).Each(func(_ int, s *goquery.Selection) {
 		if attr, ok := s.Attr(attrName); ok {
@@ -110,7 +110,7 @@ func (he HTMLElement) ChildrenAttr(selector, attrName string) []string {
 
 // Each iterates over the elements matched by the first argument
 // and calls the callback function on every HTMLElement match.
-func (he HTMLElement) Each(selector string, callback func(int, *HTMLElement)) {
+func (he *HTMLElement) Each(selector string, callback func(int, *HTMLElement)) {
 	i := 0
 	he.DOM.Find(selector).Each(func(_ int, s *goquery.Selection) {
 		for _, n := range s.Nodes {
@@ -122,7 +122,7 @@ func (he HTMLElement) Each(selector string, callback func(int, *HTMLElement)) {
 
 // Child returns the numth matched child element.
 // num starts at 1, not at 0.
-func (he HTMLElement) Child(selector string, num int) *HTMLElement {
+func (he *HTMLElement) Child(selector string, num int) *HTMLElement {
 	s := he.DOM.Find(selector)
 	nodes := s.Nodes
 	if len(nodes) == 0 {
@@ -141,17 +141,17 @@ func (he HTMLElement) Child(selector string, num int) *HTMLElement {
 }
 
 // FirstChild returns the first child element that matches the selector.
-func (he HTMLElement) FirstChild(selector string) *HTMLElement {
+func (he *HTMLElement) FirstChild(selector string) *HTMLElement {
 	return he.Child(selector, 1)
 }
 
 // LastChild returns the last child element that matches the selector.
-func (he HTMLElement) LastChild(selector string) *HTMLElement {
+func (he *HTMLElement) LastChild(selector string) *HTMLElement {
 	return he.Child(selector, -1)
 }
 
 // Parent returns the direct parent element.
-func (he HTMLElement) Parent() *HTMLElement {
+func (he *HTMLElement) Parent() *HTMLElement {
 	// If the current element is <html> tag, return nil
 	if he.Name == "html" {
 		return nil
@@ -162,7 +162,7 @@ func (he HTMLElement) Parent() *HTMLElement {
 }
 
 // Parents returns all parent elements.
-func (he HTMLElement) Parents() []*HTMLElement {
+func (he *HTMLElement) Parents() []*HTMLElement {
 	parents := make([]*HTMLElement, 0)
 
 	for {
@@ -171,7 +171,7 @@ func (he HTMLElement) Parents() []*HTMLElement {
 			break
 		}
 		parents = append(parents, parent)
-		he = *parent
+		he = parent
 	}
 
 	return parents
