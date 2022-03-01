6f3442ed36e63b6c47140462f1de218a2142bc2a
thepoy <thepoy@163.com>
2022-03-01 12:54:13 +0800

    add request methods

---
+++ methods.go
@@ -0,0 +1,22 @@
+/*
+ * @Author:    thepoy
+ * @Email:     thepoy@163.com
+ * @File Name: methods.go
+ * @Created:   2022-03-01 12:53:41
+ * @Modified:  2022-03-01 12:53:50
+ */
+
+package predator
+
+// HTTP methods were copied from net/http.
+const (
+	MethodGet     = "GET"     // RFC 7231, 4.3.1
+	MethodHead    = "HEAD"    // RFC 7231, 4.3.2
+	MethodPost    = "POST"    // RFC 7231, 4.3.3
+	MethodPut     = "PUT"     // RFC 7231, 4.3.4
+	MethodPatch   = "PATCH"   // RFC 5789
+	MethodDelete  = "DELETE"  // RFC 7231, 4.3.5
+	MethodConnect = "CONNECT" // RFC 7231, 4.3.6
+	MethodOptions = "OPTIONS" // RFC 7231, 4.3.7
+	MethodTrace   = "TRACE"   // RFC 7231, 4.3.8
+)
