07cec21d192e94ebcad71a8f7d1b9d2eae4166b4
thepoy <thepoy@163.com>
2022-11-29 16:18:28 +0800

    refactor errors.go

--- errors.go
+++ errors.go
@@ -3,42 +3,18 @@
  * @Email:     thepoy@163.com
  * @File Name: errors.go
  * @Created:   2022-02-17 15:30:54
- * @Modified:  2022-04-22 08:57:46
+ * @Modified:  2022-11-29 16:14:14
  */
 
 package predator
 
-import (
-	"fmt"
+import "errors"
+
+var (
+	ErrRequestFailed            = errors.New("request failed")
+	ErrTimeout                  = errors.New("timeout, and it is recommended to try a new proxy if you are using a proxy pool")
+	ErrInvalidCacheTypeCode     = errors.New("invalid cache type code")
+	ErrNotAllowedCacheFieldType = errors.New("only query parameters are allowed as cached fields in `GET` requests")
+	ErrNoCache                  = errors.New("no cache configured")
+	ErrInvalidResponseStatus    = errors.New("if the http status code is `302`, there must be a valid `Location` field in the response header")
 )
-
-type PredatorError uint8
-
-func (pe PredatorError) Error() string {
-	return fmt.Sprintf("PredatorError %d: %s", pe, errorMsgs[pe])
-}
-
-func (pe PredatorError) Is(e error) bool {
-	target, ok := e.(PredatorError)
-	if !ok {
-		return false
-	}
-
-	return target == pe
-}
-
-const (
-	ErrRequestFailed PredatorError = iota
-	ErrTimeout
-	ErrInvalidCacheTypeCode
-	ErrNotAllowedCacheFieldType
-	ErrNoCache
-)
-
-var errorMsgs = []string{
-	ErrRequestFailed:            "request failed",
-	ErrTimeout:                  "timeout, and it is recommended to try a new proxy if you are using a proxy pool",
-	ErrInvalidCacheTypeCode:     "invalid cache type code",
-	ErrNotAllowedCacheFieldType: "only query parameters are allowed as cached fields in `GET` requests",
-	ErrNoCache:                  "no cache configured",
-}
