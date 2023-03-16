0eb5864cdae375268f026a9875529899be47217c
thepoy <thepoy@163.com>
2023-03-16 15:31:39 +0800

    Add some test cases for the HTMLElement struct.

--- html/html_test.go
+++ html/html_test.go
@@ -3,13 +3,14 @@
  * @Email:       thepoy@163.com
  * @File Name:   html_test.go
  * @Created At:  2021-10-10 14:59:49
- * @Modified At: 2023-02-18 22:29:44
+ * @Modified At: 2023-03-16 15:26:46
  * @Modified By: thepoy
  */
 
 package html
 
 import (
+	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -18,17 +19,25 @@ import (
 var body = []byte(`<body><div id="barrierfree_container">
 <div class="top_box">
   <div class="cf top">
-    <ul class="fl top_l">
-      <li class="hide fl top_l01"><a href="" target="_blank">login</a></li>
-      <li class="hide fl top_separte"></li>
-      <li class="fl top_l02"><a href="#" target="_blank">email</a></li>
-			<li class="fl top_separte"></li>
-			<li class="fl top_l03"><a href="#" target="_blank">tg</a></li>
-			<li class="fl top_separte"></li>
-			<li id="showImg01" class="fl" style="position:relative;"><img id="showImg02" src="#"></li>
-    </ul>
-</div>
-</div>
+  <ul class="fl top_l">
+  <li class="hide fl top_l01"><a href="" target="_blank">login</a></li>
+  <li class="hide fl top_separte"></li>
+  <li class="fl top_l02"><a href="#" target="_blank">email</a></li>
+  <li class="fl top_separte"></li>
+  <li class="fl top_l03"><a href="#" target="_blank">tg</a></li>
+  <li class="fl top_separte"></li>
+  <li id="showImg01" class="fl" style="position:relative;"><img id="showImg02" src="#"></li>
+  </ul>
+  </div>
+  </div>
+  <div id="content">
+	<p>it is a P
+		<span>it is a SPAN
+			<a>it is a A</a>
+		</span>
+	</p>
+	<p>this is a P element with a long length text, and it should work in TestHtmlString</p>
+  </div>
 </body>`)
 
 func TestGetParent(t *testing.T) {
@@ -52,5 +61,67 @@ func TestGetParent(t *testing.T) {
 			So(len(parents), ShouldEqual, 7)
 			So(parents[len(parents)-1].Name, ShouldEqual, "html")
 		})
+	})
+}
+
+func TestHtmlText(t *testing.T) {
+	Convey("test to parse texts of a element", t, func() {
+		doc, err := ParseHTML(body)
+		So(err, ShouldBeNil)
+
+		e := NewHTMLElementFromSelectionNode(doc.Selection, doc.Nodes[0], 0)
+
+		So(e, ShouldNotBeNil)
+
+		excepted := `login
+email
+tg
+it is a P
+it is a SPAN
+it is a A`
+
+		So(strings.Join(e.Texts(), "\n"), ShouldEqual, excepted)
+	})
+
+	Convey("test to parse texts whose `p` and `span` are replaced with their texts of a element", t, func() {
+		doc, err := ParseHTML(body)
+		So(err, ShouldBeNil)
+
+		e := NewHTMLElementFromSelectionNode(doc.Selection, doc.Nodes[0], 0)
+
+		So(e, ShouldNotBeNil)
+
+		excepted := `login
+email
+tg
+it is a P
+it is a SPAN
+it is a A`
+
+		So(strings.Join(e.Texts(), "\n"), ShouldEqual, excepted)
+	})
+}
+
+func TestHtmlString(t *testing.T) {
+	Convey("test to stringnify a element", t, func() {
+		doc, err := ParseHTML(body)
+		So(err, ShouldBeNil)
+
+		e := NewHTMLElementFromSelectionNode(doc.Selection, doc.Nodes[0], 0)
+
+		So(e, ShouldNotBeNil)
+
+		excepted := `<div class="top_box">...</div>`
+		e1 := e.FirstChild(".top_box")
+		So(e1.String(), ShouldEqual, excepted)
+
+		excepted = "<p>thi...ing</p>"
+		e2 := e.Child("#content p", 2)
+		So(e2.String(), ShouldEqual, excepted)
+
+		excepted = `<li class="hide fl top_separte"></li>`
+		e3 := e.FirstChild(".top_separte")
+		So(e3.String(), ShouldEqual, excepted)
+
 	})
 }
