5706684a32e5f88e0468bbcc206ce5469040623f
thepoy <thepoy@163.com>
2023-03-16 09:40:24 +0800

    revise certain code comments to improve clarity and readability.

--- cache.go
+++ cache.go
@@ -3,7 +3,7 @@
  * @Email:       thepoy@163.com
  * @File Name:   cache.go
  * @Created At:  2021-11-24 20:39:11
- * @Modified At: 2023-03-15 17:53:39
+ * @Modified At: 2023-03-16 09:39:30
  * @Modified By: thepoy
  */
 
@@ -15,26 +15,32 @@ import (
 )
 
 type Cache interface {
-	// 是否开启压缩。压缩后能减小数据量，但压缩过程会耗时。
-	// 如果原数据长度很长，压缩耗时要比查询耗时低得多，此时开启压缩功能是最佳选择。
-	// 但如果原数据长度较短，压缩或不压缩，整体耗时区别不大。
-	// 是否开启压缩，需要自行测试抉择。
+	// Compressed specifies whether compression should be enabled. Compression reduces the data size, but adds processing overhead.
+	// If the original data is long, enabling compression can be the best choice because the compression overhead is lower than the query overhead.
+	// However, if the original data is short, there may be little difference in overall processing time between compressed and uncompressed data.
+	// Whether to enable compression should be determined through testing.
 	Compressed(yes bool)
-	// 初始化，用来迁移数据库 / 表，和一些与数据库有关的前期准备工作
+
+	// Init is used to migrate databases/tables and perform some database-related preparations before caching.
 	Init() error
-	// 当前请求是否已缓存过，如果缓存过，则返回缓存中的响应
+
+	// IsCached checks whether the current request has been cached, and returns the cached response if it exists.
 	IsCached(key string) ([]byte, bool)
-	// 将没有缓存过的请求保存到缓存中
+
+	// Cache saves the uncached request to the cache.
 	Cache(key string, val []byte) error
-	// 清除全部缓存
+
+	// Clear clears all cache entries.
 	Clear() error
 }
 
+// CacheModel represents the cache model which maps to the database table.
 type CacheModel struct {
 	Key   string `gorm:"primaryKey"`
 	Value []byte
 }
 
+// TableName specifies the table name for the CacheModel in the database.
 func (CacheModel) TableName() string {
 	return "predator-cache"
 }
@@ -61,27 +67,34 @@ func (cf CacheField) String() string {
 	return fmt.Sprintf("%d-%s", cf.code, cf.Field)
 }
 
+// addQueryParamCacheField is a helper function that adds a cache field to the cache key based on a query parameter.
+// It returns the cache field name and value, or an error if the query parameter is not found in the URL parameters.
 func addQueryParamCacheField(params url.Values, field CacheField) (string, string, error) {
 	if val := params.Get(field.Field); val != "" {
 		return field.String(), val, nil
 	} else {
-		// 如果设置了 cachedFields，但 url 查询参数中却没有某个 field，则报异常退出
 		return "", "", fmt.Errorf("there is no such field [%s] in the query parameters: %v", field.Field, params.Encode())
 	}
 }
 
+// NewQueryParamField creates a new CacheField instance for a query parameter.
 func NewQueryParamField(field string) CacheField {
 	return CacheField{queryParam, field, nil}
 }
 
+// NewQueryParamFieldWithPrepare creates a new CacheField instance for a query parameter with a prepare function.
+// The prepare function is used to transform the parameter value before it is included in the cache key.
 func NewQueryParamFieldWithPrepare(field string, prepare func(string) string) CacheField {
 	return CacheField{queryParam, field, prepare}
 }
 
+// NewRequestBodyParamField creates a new CacheField instance for a request body parameter.
 func NewRequestBodyParamField(field string) CacheField {
 	return CacheField{requestBodyParam, field, nil}
 }
 
+// NewRequestBodyParamFieldWithPrepare creates a new CacheField instance for a request body parameter with a prepare function.
+// The prepare function is used to transform the parameter value before it is included in the cache key.
 func NewRequestBodyParamFieldWithPrepare(field string, prepare func(string) string) CacheField {
 	return CacheField{requestBodyParam, field, prepare}
 }
